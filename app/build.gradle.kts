fdc1497a83f80cf8e1d80f0e1a361912cb294ec5
Alexei Shpikat <a.shpikat@gmail.com>
2024-12-01 12:26:26 +0300

    Init Gradle project :construction_worker:

---
+++ app/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(libs.junit.jupiter)
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
